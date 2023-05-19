1944d91f87df279a1e52d3139106bac660503dfd
RobertonManurung <robertonmanurung8@gmail.com>
2023-05-19 16:40:00 +0700

    .

--- config/database.php
+++ config/database.php
@@ -1,33 +1,11 @@
 <?php
-<?php
-$host = "localhost";
-$user = "username";
-$password = "password";
-$db = "proyek";
-
-// Membuat koneksi ke database
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-// Memeriksa koneksi
-if (mysqli_connect_errno()) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
-
-// Mengeksekusi query untuk pemanggilan data
-$query = "SELECT * FROM table_name";
-$result = mysqli_query($conn, $query);
-
-// Memeriksa hasil query
-if (!$result) {
-    die("Query tidak berhasil: " . mysqli_error($conn));
-}
-
-// Mengambil data dari hasil query
-while ($row = mysqli_fetch_assoc($result)) {
-    // Lakukan sesuatu dengan data yang diambil
-    echo $row['column_name'];
-}
-
-// Menutup koneksi
-mysqli_close($conn);
+    $host="localhost";
+    $user="root";
+    $password="";
+    $db="proyek";
+    
+    $kon = mysqli_connect($host,$user,$password,$db);
+    if (!$kon){
+          die("Koneksi gagal:".mysqli_connect_error());
+    }
 ?>
